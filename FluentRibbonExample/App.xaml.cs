a418d2009fa0500dd89ad18fcb2502b88f16b3e4
rkitamu <rkitamun@gmail.com>
2025-09-08 00:08:41 +0900

    プロジェクト ファイルを追加します。

---
+++ FluentRibbonExample/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FluentRibbonExample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
